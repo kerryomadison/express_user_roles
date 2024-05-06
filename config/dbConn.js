01870b4d430af1333f084641029df16592dd1d0c
kerryomadison <kerryomadison@gmail.com>
2024-05-06 12:04:00 -0700

    still trying to fix root

--- config/dbConn.js
+++ config/dbConn.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DATABASE_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
+            useUnifiedTopology: true
         });
         console.log('Connected to MongoDB');
     } catch (err) {
